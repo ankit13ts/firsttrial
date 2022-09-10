3c01360e1cb0986b5f822d983df46fb17f320af1
LENOVO <LENOVO@Ankit>
2022-09-10 14:32:08 +0530

    Push code trial 1

---
+++ trygit/src/module-info.java
@@ -0,0 +1,2 @@
+module trygit {
+}
---
+++ trygit/src/trygitpkg/trial1.java
@@ -0,0 +1,8 @@
+package trygitpkg;
+
+public class trial1 {
+public static void main(String[] args) {
+	System.out.println("lets make it happen");
+}
+}
+// wokrspace loc- C:\Users\LENOVO\eclipse-workspace\trygit
